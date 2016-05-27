3e31124726ca4018f7806efae50fa564c6f2e33b
Danny Trunk <dtrunk90@gmail.com>
2016-05-27 12:55:45 +0200

    fixes #1

--- src/main/java/com/github/dtrunk90/sbt/Application.java
+++ src/main/java/com/github/dtrunk90/sbt/Application.java
@@ -1,14 +1,10 @@
 package com.github.dtrunk90.sbt;
 
-import java.util.Locale;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -27,9 +23,6 @@ import com.github.mxab.thymeleaf.extras.dataattribute.dialect.DataAttributeDiale
 public class Application extends WebMvcConfigurerAdapter implements ApplicationContextAware {
 	private ApplicationContext applicationContext;
 
-	@Autowired
-	private MessageSource messageSource;
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
@@ -47,12 +40,9 @@ public class Application extends WebMvcConfigurerAdapter implements ApplicationC
 		return resolver;
 	}
 
-	private TemplateEngine templateEngine() {
-		System.out.println("### " + messageSource);
-		System.out.println("###" + messageSource.getMessage("hello", null, Locale.getDefault()));
+	@Bean
+	public TemplateEngine templateEngine() {
 		SpringTemplateEngine engine = new SpringTemplateEngine();
-		//engine.setMessageSource(messageSource);
-		//engine.setTemplateEngineMessageSource(messageSource);
 		engine.setEnableSpringELCompiler(true);
 		engine.setTemplateResolver(templateResolver());
 		engine.addDialect(new DataAttributeDialect());
--- src/main/java/com/github/dtrunk90/sbt/controller/IndexController.java
+++ src/main/java/com/github/dtrunk90/sbt/controller/IndexController.java
@@ -1,18 +1,11 @@
 package com.github.dtrunk90.sbt.controller;
 
-import java.util.Locale;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-	@Autowired
-	private MessageSource messageSource;
-
 	@RequestMapping
 	public String root() {
 		return "redirect:index.html";
@@ -20,8 +13,6 @@ public class IndexController {
 
 	@RequestMapping("index.html")
 	public String index(ModelMap model) {
-		System.out.println("##### " + messageSource);
-		System.out.println("#####" + messageSource.getMessage("hello", null, Locale.getDefault()));
 		model.addAttribute("hello", "world");
 		return "index";
 	}
